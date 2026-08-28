61b21c95cf7e5157b93fc4a3edbbdecba67bbe09
Shanwi5 <shanwibm@gmail.com>
2026-08-28 15:09:37 +0530

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api"
+    "https://cab-booking-system-izqy.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
